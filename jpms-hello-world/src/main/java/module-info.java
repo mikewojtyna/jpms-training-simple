62bd59f2def8307e26af4a086e5a4dc9859e4a5a
Mike Wojtyna <mike@buildmysoftware.pro>
2019-11-06 19:20:26 +0100

    Create hello world module

---
+++ jpms-hello-world/src/main/java/module-info.java
@@ -0,0 +1,2 @@
+module pro.buildmysoftware.helloworld {
+}
---
+++ jpms-hello-world/src/main/java/pro/buildmysoftware/helloworld/Main.java
@@ -0,0 +1,9 @@
+package pro.buildmysoftware.helloworld;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World from module: " + Main.class
+			.getModule());
+	}
+}
